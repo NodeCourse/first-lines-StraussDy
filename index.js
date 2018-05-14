118e4d99d3798f93436dd686b249188c591a91a8
Unknown <clauss.dylan@gmail.com>
2018-05-14 10:31:23 +0200

    Second commit
    + index.js
    + test.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+setInterval(write, 1000);
+
+function write() {
+    const actual = Date.now();
+    console.log(actual);
+
+    fs.appendFileSync('message.txt', actual + "\n");
+
+}
